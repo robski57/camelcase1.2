2c95617572f5109181bff273fcb615807e567141
robski57 <robski577@gmail.com>
2016-02-18 15:59:28 -0600

    Added files via upload
    Working camelcase

---
+++ camelCase1.0.py
@@ -0,0 +1,11 @@
+import re
+
+ppl=input("Enter your sentence: " )
+ppl=ppl.title()
+print(ppl)
+
+#def convert(name):
+   
+    #s1=re.sub('(.)(a-z[A-Z]+)',r'\1_\2',ppl)
+    #return re.sub('([a-z0-9])([A-Z])',r'\1_\2',s1).title()
+
